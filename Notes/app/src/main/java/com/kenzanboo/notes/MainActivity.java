9bfebc93dbad0bbb27b5acd8295b797407fc1f4b
Kenzan Boo <kenzanboo@gmail.com>
2015-06-09 16:20:49 -0700

    notes project

---
+++ Notes/app/src/main/java/com/kenzanboo/notes/MainActivity.java
@@ -0,0 +1,61 @@
+package com.kenzanboo.notes;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private NoteListItemAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new NoteListItemAdapter(this, mRecyclerView);
+        mRecyclerView.setAdapter(mAdapter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    getApplicationContext().getString(R.string.hello_world),
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ Notes/app/src/main/java/com/kenzanboo/notes/NoteListItem.java
@@ -0,0 +1,13 @@
+package com.kenzanboo.notes;
+
+public class NoteListItem {
+    private String text;
+
+    public NoteListItem(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+}
