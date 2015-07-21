0d661d40585efe5d9229f6ba9c69f22621f16b2e
Kenzan Boo <kenzanboo@gmail.com>
2015-07-21 16:24:18 -0700

    Alarm code

--- NotificationsAndServices/app/src/main/java/com/kenzanboo/notificationsandservices/MainActivity.java
+++ NotificationsAndServices/app/src/main/java/com/kenzanboo/notificationsandservices/MainActivity.java
@@ -1,10 +1,12 @@
 package com.kenzanboo.notificationsandservices;
 
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -13,23 +15,68 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Calendar;
+
 
 public class MainActivity extends ActionBarActivity {
     private int notificationID = 100;
+    private int ALARM_TIMER = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button notificationButton = (Button) findViewById(R.id.button);
+        initializeButtons();
+    }
+
+    private void initializeButtons() {
+        Button notificationButton = (Button) findViewById(R.id.notify);
         notificationButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 displayNotification();
             }
         });
+        Button alarmButton = (Button) findViewById(R.id.setAlarm);
+        alarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setAlarm();
+            }
+        });
+        Button cancelAlarmButton = (Button) findViewById(R.id.cancelAlarm);
+        cancelAlarmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
+            }
+        });
     }
+
+    protected void setAlarm() {
+        AlarmManager alarmMgr = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+//        alarmMgr.set(AlarmManager.ELAPSED_REALTIME,
+//                SystemClock.elapsedRealtime() + ALARM_TIMER * 1000,
+//                getMainActivityPendingIntent());
+        alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + ALARM_TIMER * 1000,
+                ALARM_TIMER * 1000,
+                getMainActivityPendingIntent());
+
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTimeInMillis(System.currentTimeMillis());
+//        calendar.set(Calendar.HOUR_OF_DAY, 16);
+//        calendar.set(Calendar.MINUTE, 25);
+//        long milliseconds = calendar.getTimeInMillis();
+//        alarmMgr.setInexactRepeating(AlarmManager.RTC,
+//                milliseconds,
+//                AlarmManager.INTERVAL_DAY,
+//                getMainActivityPendingIntent());
+    }
+    protected void cancelAlarm() {
+        AlarmManager alarmMgr = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        alarmMgr.cancel(getMainActivityPendingIntent());    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
